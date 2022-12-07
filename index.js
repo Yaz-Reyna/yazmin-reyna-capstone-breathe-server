29bc98710750e16490c79e6b6a3e88ebe547eb5c
Yaz Reyna <yazreyna@MacBook-Pro-de-Yaz.local>
2022-12-07 16:30:32 -0500

    Install knex, mysql. Create migration file. Create seeds file. Create database from seeds

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5050;
 
 app.get('/', (request, response) => {
     response.send("Hello?");
---
+++ migrations/20221207203839_create_quotes_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("quotes", (table) => {
+        table.increments('id');
+        table.string('text').notNullable();
+        table.string('author').notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("quotes");
+};
